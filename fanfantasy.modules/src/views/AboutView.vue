1a359cb589144347a72293af9d6e332263bc7fc5
Thiago <thiago.carreiro@hotmail.com>
2024-07-20 04:34:06 -0300

    Football loader created. Features branchs from now on

--- fanfantasy.modules/src/views/AboutView.vue
+++ fanfantasy.modules/src/views/AboutView.vue
@@ -1,15 +1,14 @@
+<script setup lang="ts">
+import FootballLoader from '@/components/Loader/FootballLoader.vue';
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <FootballLoader/>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  display: flex;
+  align-items: center;
 }
 </style>
