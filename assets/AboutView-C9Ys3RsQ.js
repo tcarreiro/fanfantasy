dd12d80895470adec7ccb805d0da71edddff23a2
Thiago <thiago.carreiro@hotmail.com>
2024-07-20 04:34:54 -0300

    Updates

--- assets/AboutView-C9Ys3RsQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t,a}from"./index-DGPX8d-u.js";const o={},n={class:"about"},c=a("h1",null,"This is an about page",-1),_=[c];function r(i,f){return s(),t("div",n,[..._])}const u=e(o,[["render",r],["__file","C:/Users/thiag/Projects/Fanfantasy/Frontend/fanfantasy/fanfantasy.modules/src/views/AboutView.vue"]]);export{u as default};
