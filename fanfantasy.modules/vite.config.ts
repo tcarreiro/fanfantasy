e57096299e712fd8299f5cbd7fac255e258bbb2d
Thiago <thiago.carreiro@hotmail.com>
2024-07-22 10:24:45 -0300

    production env path added

--- fanfantasy.modules/vite.config.ts
+++ fanfantasy.modules/vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({mode}) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: mode === 'production' ? '/fanfantasy/' : '/'
   }
 });
